258597456dde83de1f868155addda9623b3fd378
PmpPiyas <princemahmudpiyas@gmail.com>
2026-01-09 00:47:34 +0600

    add: create payment ui

--- src/app/(dasboardLayout)/admin/dashboard/payments/page.tsx
+++ src/app/(dasboardLayout)/admin/dashboard/payments/page.tsx
@@ -1,9 +1,22 @@
 import PaymentContent from '@/app/components/module/dashboard/admin/payment/PaymentContent';
+import { getClasses } from '@/app/services/class/getAllClasses';
+import { getAllFeeTypes } from '@/app/services/fee/getFeetypes';
+import { getAllStudents } from '@/app/services/student/getAllStudents';
+import { getCookie } from '@/lib/cookies';
 
-const PaymentPage = () => {
+const PaymentPage = async () => {
+  const { classes } = await getClasses();
+  const classId = await getCookie('selectedClassId');
+  const { students } = await getAllStudents(classId as string);
+
+  const feetypes = await getAllFeeTypes();
   return (
     <div>
-      <PaymentContent />
+      <PaymentContent
+        classes={classes}
+        students={students}
+        feetypes={feetypes}
+      />
     </div>
   );
 };
--- src/app/components/module/dashboard/admin/payment/PaymentContent.tsx
+++ src/app/components/module/dashboard/admin/payment/PaymentContent.tsx
@@ -2,9 +2,20 @@
 import CreatePayment from '@/app/components/module/dashboard/admin/payment/CreatePayment';
 import PaymentHeader from '@/app/components/module/dashboard/admin/payment/PaymentHeader';
 import PaymentModeSelect from '@/app/components/module/dashboard/admin/payment/PaymentModeSelect';
+import { IClass } from '@/types/attendance.interface';
+import { IFeeType } from '@/types/fee.interface';
+import { IStudent } from '@/types/student.interface';
 import { useState } from 'react';
 
-const PaymentContent = () => {
+const PaymentContent = ({
+  classes,
+  students,
+  feetypes
+}: {
+  classes: IClass[];
+  students: IStudent[];
+    feetypes: IFeeType[];
+}) => {
   const [mode, setMode] = useState<'create' | 'view'>('create');
   return (
     <div>
@@ -12,7 +23,9 @@ const PaymentContent = () => {
         selectMode={<PaymentModeSelect mode={mode} onChange={setMode} />}
       />
 
-      {mode === 'create' && <CreatePayment />}
+      {mode === 'create' && (
+        <CreatePayment classes={classes} students={students}  feetypes={feetypes}/>
+      )}
       {mode === 'view' && <p>View</p>}
     </div>
   );
